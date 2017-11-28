6a5d5b36d2bbd23aace83662236cc70cc60a0cee
dk67604 <dk67604@uga.edu>
2017-11-27 20:50:51 -0500

    New changes

--- client/routes/users.js
+++ client/routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var AWS = require('aws-sdk');
 var fs=require('fs');
+var request = require('request');
 var multer  = require('multer')
 var upload = multer().single('avatar')
+
+
 
 var bucketName = 'advanced-data-analytics';
 //var keyName = 'hello_world.txt';
@@ -39,8 +42,36 @@ router.post('/upload',function (req,res,next) {
 
 });
 
+var evaluateModel=function (endpoint) {
+    return new Promise(function (resolve,reject) {
+        var options = {
+            uri: 'https://yvrq6r0cb8.execute-api.us-east-2.amazonaws.com/test/objectdetection',
+            method: 'POST',
+            json: {
+                "url":endpoint
+            }
+        };
+        request(options, function (error, response, body) {
+            //console.log(response);
+            if (!error && response.statusCode == 200) {
+                resolve(response);
+            }
+            else{
+                reject(error);
+            }
+        });
+    });
+}
+
 router.post('/evaluate',function (req,res,next) {
-    console.log(req.session.filename);
-    res.send("Success");
+    var baseUrl='https://s3.amazonaws.com/advanced-data-analytics/';
+
+    var url=baseUrl+req.session.filename;
+    console.log(url);
+    evaluateModel(url).then(function (result) {
+        res.send("Success");
+    }).catch(function (error) {
+       res.send('Fail to preprocess image,try later');
+    });
 });
 module.exports = router;
